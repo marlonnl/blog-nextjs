fd3d9886f03bdef58518e2d687b7db8a5fda54ab
Marlonn Locatelli <marlonn@gmail.com>
2026-09-07 15:06:43 -0300

    ✨ added Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+export function Header() {
+  return (
+    <header>
+      <h1>
+        <h1
+          className="
+          font-extrabold
+          text-5xl/tight
+          py-8
+
+          sm:text-5xl/normal
+          sm:py-10
+
+          md:text-6xl/normal
+          md:py-11
+
+          lg:text-7xl/normal
+          lg:py-12
+          "
+        >
+          <a href="#">Blog</a>
+        </h1>
+      </h1>
+    </header>
+  )
+}
