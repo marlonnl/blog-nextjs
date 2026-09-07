363576bed9c1a558c50c7d5cbdc54c351e7afe98
Marlonn Locatelli <marlonn@gmail.com>
2026-09-07 14:20:25 -0300

    🧪 SIMULATE_LOADING_TIME logic for testing

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -7,7 +7,14 @@ import { PostRepository } from "./post-repository"
 const ROOT_DIR = process.cwd()
 const JSON_POSTS_PATH = resolve(ROOT_DIR, "src", "db", "seed", "posts.json")
 
+const SIMULATE_LOADING_TIME = 0
+
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_LOADING_TIME <= 0) return
+    await new Promise(resolve => setTimeout(resolve, SIMULATE_LOADING_TIME))
+  }
+
   private async readFromDisk(): Promise<PostModel[]> {
     const jsonPostContent = await readFile(JSON_POSTS_PATH, "utf-8")
     const postContentParsed = JSON.parse(jsonPostContent)
@@ -17,11 +24,15 @@ export class JsonPostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
+    await this.simulateWait()
+
     const posts = await this.readFromDisk()
     return posts
   }
 
   async findById(id: string): Promise<PostModel> {
+    await this.simulateWait()
+
     const posts = await this.readFromDisk()
     const post = posts.find(post => post.id === id)
 
