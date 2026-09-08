aaa70c6d95a0086f035b8cbc623dcaaa0d20a404
Marlonn Locatelli <marlonn@gmail.com>
2026-09-08 19:23:13 -0300

    💄 styling

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,12 +1,38 @@
 import { postRepository } from "@/repositories/post"
+import PostCoverImage from "../PostCoverImage"
+import { Title } from "../Title"
 
 export default async function PostsList() {
   const posts = await postRepository.findAll()
 
   return (
-    <div>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {posts.map(post => {
-        return <p key={post.id}>{post.title}</p>
+        const postLink = `/post/${post.slug}`
+
+        return (
+          <div key={post.id} className="flex flex-col gap-4 group">
+            <PostCoverImage
+              linkProps={{ href: postLink }}
+              imageProps={{
+                alt: `Ler ${post.title}`,
+                width: 1200,
+                height: 720,
+                src: post.coverImageUrl,
+              }}
+            />
+            <div className="flex flex-col sm:justify-center">
+              <time
+                dateTime={post.createdAt}
+                className="text-slate-500 text-sm/snug"
+              >
+                {post.createdAt}
+              </time>
+              <Title url={postLink}>{post.title}</Title>
+              <p>{post.excerpt}</p>
+            </div>
+          </div>
+        )
       })}
     </div>
   )
--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -8,18 +8,19 @@ type TitleProp = {
 
 export function Title({ children, url, as: ElementTitle = "h2" }: TitleProp) {
   const TitleClassesCSS = {
-    h1: "text-3xl/normal md:text-4xl/normal",
-    h2: "text-2xl/tight md:text-3xl/normal",
+    h1: "text-3xl/normal md:text-4xl/normal font-extrabold",
+    h2: "text-2xl/tight md:text-2xl/tight font-bold",
   }
 
   return (
     <ElementTitle
       className={`
-        font-extrabold
         md:mb-4
         ${TitleClassesCSS[ElementTitle]}`}
     >
-      <Link href={url}>{children}</Link>
+      <Link href={url} className="group-hover:text-slate-600 transition">
+        {children}
+      </Link>
     </ElementTitle>
   )
 }
