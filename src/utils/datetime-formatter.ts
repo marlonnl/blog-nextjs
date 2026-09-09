4ca46b261b4451aedb7be6ec6d8208537b430393
Marlonn Locatelli <marlonn@gmail.com>
2026-09-09 16:05:37 -0300

    💄 changed formatting

--- src/utils/datetime-formatter.ts
+++ src/utils/datetime-formatter.ts
@@ -3,7 +3,7 @@ import { ptBR } from "date-fns/locale"
 
 export function datetimeFormatter(rawDatetime: string): string {
   const date = new Date(rawDatetime)
-  return format(date, "HH:mm - dd/MM/yyyy", { locale: ptBR })
+  return format(date, "dd/MM/yyyy - HH:mm", { locale: ptBR })
 }
 
 export function relativeDatatimeFormatter(rawDatetime: string): string {
