e5b29b2a283ceebabb4e1280d1124d17ac2a540b
Marlonn Locatelli <marlonn@gmail.com>
2026-09-12 14:27:45 -0300

    ✨ added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+export default function NotFoundPage() {
+  return (
+    <div
+      className="
+      min-h-80
+      bg-slate-900
+      text-slate-100
+      mb-16
+      p-8
+      rounded-xl
+      flex items-center justify-center
+      text-center
+    "
+    >
+      <div>
+        <h1 className="text-7xl/tight mb-6 font-extrabold">404</h1>
+        <p>Erro: a página não existe.</p>
+      </div>
+    </div>
+  )
+}
