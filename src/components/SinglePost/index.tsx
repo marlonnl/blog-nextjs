4639e18017c39f9879825ead24791ac50ee0aafc
Marlonn Locatelli <marlonn@gmail.com>
2026-09-16 15:34:08 -0300

    ✨ feat: new component SinglePost

--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -1,4 +1,7 @@
 import { findPostBySlugCached } from "@/lib/posts/queries"
+import Image from "next/image"
+import { Title } from "../Title"
+import PostDate from "../PostDate"
 
 type SinglePostProps = {
   slug: string
@@ -7,5 +10,28 @@ type SinglePostProps = {
 export default async function SinglePost({ slug }: SinglePostProps) {
   const post = await findPostBySlugCached(slug)
 
-  return <p>{post.content}</p>
+  return (
+    <article className="mb-16">
+      <header className="group flex flex-col gap-4 mb-4">
+        <Image
+          className="rounded-xl"
+          src={post.coverImageUrl}
+          alt={post.title}
+          width={1200}
+          height={720}
+          priority
+        />
+
+        <Title url={`/post/${post.slug}`}>{post.title}</Title>
+
+        <p>
+          {post.author} | <PostDate dateTime={post.createdAt} />
+        </p>
+      </header>
+
+      <p className="text-xl/tight text-slate-600 mb-8 mx-6">{post.excerpt}</p>
+
+      <div>{post.content}</div>
+    </article>
+  )
 }
