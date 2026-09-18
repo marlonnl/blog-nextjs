261db88262a0b0e38d06d1157f105857a45915d1
Marlonn Locatelli <marlonn@gmail.com>
2026-09-18 17:14:02 -0300

    ✨ feat: new component ErrorMessage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,11 @@
+import ErrorMessage from "@/components/ErrorMessage"
+
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Página não encontrada</title>
-      <div
-        className="
-      min-h-80
-      bg-slate-900
-      text-slate-100
-      mb-16
-      p-8
-      rounded-xl
-      flex items-center justify-center
-      text-center
-      "
-      >
-        <div>
-          <h1 className="text-7xl/tight mb-6 font-extrabold">404</h1>
-          <p>Erro: a página não existe.</p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle="Página não encontrada"
+      contentTitle="404 😅"
+      content="Erro: a página não existe."
+    />
   )
 }
